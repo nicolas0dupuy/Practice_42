e9e88ab8442cd7dd079f56f3b2cfdfe73ff6dc22
Nicolas Dupuy <nicolas@nicolas-u.home>
2022-04-18 15:16:02 +0200

    Implemented dichotomy ordering for ex08

--- c01/ex08/ft_sort_int_tab.c
+++ c01/ex08/ft_sort_int_tab.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 void ft_sort_int_tab(int *tab, int size)
 {
     if (size > 1)
@@ -7,18 +9,54 @@ void ft_sort_int_tab(int *tab, int size)
     }
     int i = 0;
     int j = size/2;
-    while (i < size/2)
+    while ((i < j) && (j < size))
     {
-        if (tab[i] < tab[j])
+        if (tab[i] <= tab[j])
         {
             i++;
             continue;
         }
         else
         {
-            // Compute how much to shift from j
-            // prepare an arrat to store the too big i.s
-            // perform the shift
+            int tmp = tab[i];
+            tab[i] = tab[j];
+            int k = j;
+            while (k > i + 1)
+            {
+                tab[k] = tab[k - 1];
+                k--;
+            }
+            tab[i+1] = tmp;
         }
+        j++;
+        i++;
     }
+}
+
+int main()
+{
+    int tab[10] = {2, 1, 3, 9, 5, 4, 8, 6, 4, 2};
+    int tab2[11] = {2, 1, 3, 9, 5, 7, 8, 6, 4, 2, 4};
+    for(int i = 0; i < 10; i++)
+    {
+        printf("%d ", tab[i]);
+    }
+    printf("\n");
+    ft_sort_int_tab(tab, 10);
+    for(int i = 0; i < 10; i++)
+    {
+        printf("%d ", tab[i]);
+    }
+    printf("\n");
+    for(int i = 0; i < 11; i++)
+    {
+        printf("%d ", tab2[i]);
+    }
+    printf("\n");
+    ft_sort_int_tab(tab2, 11);
+    for(int i = 0; i < 11; i++)
+    {
+        printf("%d ", tab2[i]);
+    }
+    printf("\n");
 }
